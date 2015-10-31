b093249eb84e11ca3096e2b01f223e4506b585d1
Angela <angelaung29@gmail.com>
2015-11-01 02:06:42 +1100

    Fix: .js

--- edit.php
+++ edit.php
@@ -4,8 +4,11 @@
     <title></title>
     <?php include 'widgets/common.php'; ?>
     <script src="js/edit.js"></script>
+    <script type="text/template" class="edit-template">
+        <?php include 'template/edit.php'; ?>
+    </script>
 </head>
-<body class="">
+<body>
     <div class="container">
         <div class="page-header">
             <h1>Moderate Tenders</h1>
@@ -13,9 +16,6 @@
         <div class="row">
             <div>
                 <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                    <script type="text/template" class="edit-template">
-                       <?php include 'template/edit.php'; ?>
-                    </script>
                 </div>
             </div>
         </div>
--- js/edit.js
+++ js/edit.js
@@ -1,4 +1,5 @@
 $(function() {
+
 	//Load the tenders for editting
 	$.ajax({
 		url: '/api/tenders.php',
@@ -7,51 +8,54 @@ $(function() {
 		success: function(response) {
 			if(response.success) {
 				var template = _.template($('script.edit-template').html());
-				console.log('*******', response.success);
-		        $('.panel-group').html(template({tenders: response.success}));
+				$('.panel-group').html(template({tenders: response.success}));
+
+				//Listen for EDIT
+				$('.edit-btn').click(function(event) {
+					$(this).closest('.tender-panel').attr('data-edit-mode', 1);
+				});
+
+				//Listen for CANCEL
+				$('.cancel-btn').click(function(event) {
+					$(this).closest('.tender-panel').attr('data-edit-mode', 0);
+				});
+
+
+				//Listen for SUBMIT
+				$('.edit-tender').submit(function(event) {
+					event.preventDefault();
+
+					$.ajax({
+						url: '/api/edit.php',
+						type: 'POST',
+						dataType: "json",
+						data: [$(this).serialize(), $.param({id: $(this).find('.tender-panel').attr('data-tender-id')})].join('&'),
+						success: function(response) {
+							if(response.success) {
+								var fields = $(event.target).find(':input').serializeArray();
+								$.each(fields, function(i, field) {
+									$(event.target).find('.' + field.name + '-text').html(field.value);
+								});
+								$(event.target).find('.tender-panel').attr('data-edit-mode', 0);
+								return $(event.target).find('.edit-message').addClass('alert alert-success').html(response.success);
+							}
+
+							if(response.errors) {
+								return $(event.target).find('.edit-message').addClass('alert alert-danger').html(response.errors);
+							}	
+						}
+					});
+
+				});
+
 				return;
 			}
 
 			if(response.errors) {
 				return;
 			}	
+
+			return;
 		}
-	});
-
-
-	$('.edit-btn').click(function(event) {
-		$(event.target).closest('.tender-panel').attr('data-edit-mode', 1);
-	});
-
-
-	$('.cancel-btn').click(function(event) {
-		$(event.target).closest('.tender-panel').attr('data-edit-mode', 0);
-	});
-
-
-	$('.edit-tender').submit(function(event) {
-		event.preventDefault();
-
-		$.ajax({
-			url: '/api/edit.php',
-			type: 'POST',
-			dataType: "json",
-			data: [$(this).serialize(), $.param({id: $(this).find('.tender-panel').attr('data-tender-id')})].join('&'),
-			success: function(response) {
-				if(response.success) {
-					var fields = $(event.target).find(':input').serializeArray();
-					$.each(fields, function(i, field) {
-						$(event.target).find('.' + field.name + '-text').html(field.value);
-					});
-					$(event.target).find('.tender-panel').attr('data-edit-mode', 0);
-					return $(event.target).find('.edit-message').addClass('alert alert-success').html(response.success);
-				}
-
-				if(response.errors) {
-					return $(event.target).find('.edit-message').addClass('alert alert-danger').html(response.errors);
-				}	
-			}
-		});
-
 	});
 });
